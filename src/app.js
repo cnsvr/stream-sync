091c9c74a4ec1c0a8c889ed9fcd5b442987378ad
furkancansever <furkancansever@imaginitesoft.com>
2024-05-22 16:28:02 +0300

    fix: remove path from peerjs

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
 require("dotenv").config();
-const { Server } = require("socket.io");
 const { ExpressPeerServer } = require('peer');
 
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
 
 // Middleware
 app.use(express.json());
@@ -18,6 +16,7 @@ app.use(cors());
 // PeerJS sunucusunu oluşturma
 const peerServer = ExpressPeerServer(server, {
   debug: true,
+  path: '/myapp'
 });
 app.use('/peerjs', peerServer);
 
@@ -36,28 +35,6 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-io.on('connection', (socket) => {
-  socket.on('joinMeeting', ({ meetingId, userId }) => {
-    socket.join(meetingId);
-    console.log(`User ${userId} joined meeting ${meetingId}`);
-    socket.broadcast.to(meetingId).emit('userJoined', { userId });
-
-    socket.on('sendVideo', (data) => {
-      console.log('Received video data:', data);
-      socket.broadcast.to(meetingId).emit('receiveVideo', data);
-      console.log('Sent video data to other users');
-    });
-
-    socket.on('disconnect', (reason) => {
-      console.log('Client disconnected:', reason);
-      socket.broadcast.to(meetingId).emit('userDisconnected', { userId });
-    });
-  });
-
-  socket.on('connect_error', (err) => {
-    console.error('Connection error:', err);
-  });
-});
 
 // Connect to MongoDB
 mongoose
